b6e6fa480d682f927c9f38b897ba598dd3fa6a7d
UeberallGebannt <paulmertens63@gmail.com>
2022-08-08 15:12:58 +0200

    Include all libraries using gradle

---
+++ src/net/kettlemc/plugin/Libraries.java
@@ -0,0 +1,11 @@
+package net.kettlemc.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Libraries extends JavaPlugin {
+
+    public void onEnable() {
+        getLogger().info("This plugin contains multiple libraries for KettleMC.net");
+    }
+
+}
